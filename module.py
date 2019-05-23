e00a6e8fcc96d48bf137d69ab288ad02b385b666
manuela98 <manujarare@hotmail.com>
2019-05-23 13:20:28 -0500

    code smells

--- module.py
+++ module.py
@@ -2,33 +2,32 @@ import numpy
 
 class Series(object):
 
-    def __init__(self,N,a):
-        self.N=N
+    def __init__(self,nterms,a):
+        self.nterms=nterms
         self.a=a
-        n=len(a)
-        
+
     def get_value(self,x):
         suma=0
-        for n in range(self.N):
+        for n in range(self.nterms):
             suma+=self.a[n]*x**n
         return suma
 
     def get_derivative(self,x):
         deriv=0
-        for n in range(1,self.N):
+        for n in range(1,self.nterms):
             deriv+=n*self.a[n]*x**(n-1)
         return deriv
 
-    def getError(self,x,n):
+    def get_error(self,x,n):
         if n<0:
             raise ValueError("n should be larger than 0")
-        elif n>self.N-1:
-            raise ValueError("n should not be larger than {self.N}")
+        elif n>self.nterms-1:
+            raise ValueError("n should not be larger than {self.nterms}")
         else:
             error=x**(n+1)
             return error
 
-    def Clean(self):
+    def clean(self):
         #This funtion is only for seeing is system works
         from os import system
         system("ls 2> /dev/null")
